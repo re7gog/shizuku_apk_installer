7051adebbf385e803796b1d2e150fbba3965ad19
Gregory Velichko <re7gog@gmail.com>
2024-04-01 16:37:16 +0300

    Libs

---
+++ android/src/main/kotlin/dev/re7gog/shizuku_apk_installer/util/ApplicationUtils.java
@@ -0,0 +1,37 @@
+package dev.re7gog.shizuku_apk_installer.util;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.os.Build;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ApplicationUtils {
+
+    private static Application application;
+
+    public static Application getApplication() {
+        return application;
+    }
+
+    public static void setApplication(Application application) {
+        ApplicationUtils.application = application;
+    }
+
+    @SuppressWarnings({"JavaReflectionMemberAccess"})
+    @SuppressLint({"PrivateApi", "DiscouragedPrivateApi"})
+    public static String getProcessName() {
+        if (Build.VERSION.SDK_INT >= 28)
+            return Application.getProcessName();
+        else {
+            try {
+                Class<?> activityThread = Class.forName("android.app.ActivityThread");
+                Method method = activityThread.getDeclaredMethod("currentProcessName");
+                return (String) method.invoke(null);
+            } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ android/src/main/kotlin/dev/re7gog/shizuku_apk_installer/util/IIntentSenderAdaptor.java
@@ -0,0 +1,25 @@
+package dev.re7gog.shizuku_apk_installer.util;
+
+import android.content.IIntentReceiver;
+import android.content.IIntentSender;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public abstract class IIntentSenderAdaptor extends IIntentSender.Stub {
+
+    public abstract void send(Intent intent);
+
+    @Override
+    public int send(int code, Intent intent, String resolvedType,
+                    IIntentReceiver finishedReceiver, String requiredPermission, Bundle options) {
+        send(intent);
+        return 0;
+    }
+
+    @Override
+    public void send(int code, Intent intent, String resolvedType, IBinder whitelistToken,
+                     IIntentReceiver finishedReceiver, String requiredPermission, Bundle options) {
+        send(intent);
+    }
+}
